0ccfa5b7041f4d6f004166e95a5555965e28e35e
Matteo Puccio <82228938+MatteoPuccio@users.noreply.github.com>
2024-05-21 15:31:51 +0200

    Made Padlock model and created animation for it

---
+++ VRProject/Assets/Scripts/Padlock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Padlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var _animator = GetComponent<Animator>();
+        //_animator.SetBool("Unlock", true);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
