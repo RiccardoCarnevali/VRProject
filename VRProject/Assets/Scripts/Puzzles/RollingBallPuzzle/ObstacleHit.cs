605f8b32f52f4589d573ce5167149a1a25b9658d
Riccardo Carnevali <82229671+RiccardoCarnevali@users.noreply.github.com>
2024-05-24 15:32:29 +0200

    Obstacles now appear for a brief moment after being hit

--- VRProject/Assets/Scripts/Puzzles/RollingBallPuzzle/ObstacleHit.cs
+++ VRProject/Assets/Scripts/Puzzles/RollingBallPuzzle/ObstacleHit.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class ObstacleHit : MonoBehaviour
 {
+    [SerializeField] private LayerMask visibleLayer;
+    [SerializeField] private LayerMask invisibleLayer;
+
+    private float visibleTime = 1;
+
     private void OnCollisionEnter(Collision collision) {
-        if (collision.collider.tag == "Ball") {
-            Debug.DrawLine(collision.transform.position, collision.transform.TransformPoint(collision.rigidbody.velocity).normalized, Color.green, 2);
-            // if (GetComponent<Collider>().bounds.Contains(collision.transform.TransformPoint(collision.rigidbody.velocity))) {
-            //     Debug.Log("yes");
-            // }
+        if (Vector3.Dot(collision.rigidbody.velocity.normalized, collision.contacts[0].normal.normalized) > 0.8) {
+            StartCoroutine(MakeVisible());
         }
+    }
+
+    private IEnumerator MakeVisible() {
+        float visibleProgress = 0;
+        gameObject.layer = (int) Mathf.Log(visibleLayer.value, 2);
+        Renderer renderer = GetComponent<Renderer>();
+        Color startColor = new Color(1, 1, 1, 0.8f);
+        Color endColor = new Color(1, 1, 1, 0);
+
+        while (visibleProgress < visibleTime) {
+            visibleProgress += Time.deltaTime * Time.timeScale;
+            renderer.material.color = Color.Lerp(startColor, endColor, visibleProgress / visibleTime);
+            yield return null;
+        }
+
+        gameObject.layer = (int) Mathf.Log(invisibleLayer.value, 2);
+        renderer.material.color = Color.white;
     }
 }
