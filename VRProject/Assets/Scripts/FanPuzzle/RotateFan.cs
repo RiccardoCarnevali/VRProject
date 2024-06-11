c8e12b1cfa14b47ff473c2c5d4f3bfb3e9086ffb
Noemi Ruffolo <ruffolo.noemi@gmail.com>
2024-06-11 22:14:02 +0200

    Added Fan Puzzle in Area 2

--- VRProject/Assets/Scripts/FanPuzzle/RotateFan.cs
+++ VRProject/Assets/Scripts/FanPuzzle/RotateFan.cs
@@ -1,11 +1,23 @@
+using HighlightPlus;
 using UnityEngine;
 
-public class RotateObject : MonoBehaviour
-{
-    public float rotationSpeed = 90.0f; 
+public class RotateFan : CameraAffected {
+    [SerializeField] private float rotationSpeed = 90.0f;
 
-    void Update()
-    {
+    void Start() {
+        if (Settings.load && SaveSystem.CheckFlag("fan_puzzle_done")) {
+            enabled = false;
+            GetComponent<HighlightEffect>().enabled = false;
+        }
+    }
+
+    protected override void OnCameraAffected() {
+        enabled = false;
+        GetComponent<HighlightEffect>().enabled = false;
+        SaveSystem.SetFlag("fan_puzzle_done");
+    }
+
+    void Update(){
         transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
     }
 }
